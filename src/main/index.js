4e9405997add686462dd0ea665d222f6bd5ae74c
陆志洁 <luzhijie@MacBook-Pro.local>
2021-10-29 21:22:36 +0800

    修复mac上关闭程序的报错问题

--- src/main/index.js
+++ src/main/index.js
@@ -5,48 +5,51 @@ import {app, BrowserWindow} from 'electron'
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-    global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
 }
 
 let mainWindow
+let intervalKey
 const winURL = process.env.NODE_ENV === 'development'
     ? `http://localhost:9080`
     : `file://${__dirname}/index.html`
 
 function createWindow() {
-    /**
-     * Initial window options
-     */
-    mainWindow = new BrowserWindow({
-        height: 800,
-        useContentSize: true,
-        width: 500,
-        minWidth: 500,
-    })
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    height: 800,
+    useContentSize: true,
+    width: 500,
+    minWidth: 500,
+  })
 
-    mainWindow.loadURL(winURL)
+  mainWindow.loadURL(winURL)
 
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
 
-    setInterval(() => {
-        mainWindow.webContents.send('checkClipboard', 'ping');
-    }, 100)
+  intervalKey = setInterval(() => {
+    if(mainWindow) {
+      mainWindow.webContents.send('checkClipboard', 'ping');
+    }
+  }, 100)
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
-    }
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
 app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow()
-    }
+  if (mainWindow === null) {
+    createWindow()
+  }
 })
 
 /**
@@ -67,4 +70,4 @@ autoUpdater.on('update-downloaded', () => {
 app.on('ready', () => {
   if (process.env.NODE_ENV === 'production') autoUpdater.checkForUpdates()
 })
- */
+*/
